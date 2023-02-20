4fba15018e7714a682884affebf0b7b392b3636b
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-02-20 12:18:40 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.4.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.0.0"
+      version = ">= 2.2.0"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
