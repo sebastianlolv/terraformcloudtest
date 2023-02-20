a4de9176fc34ee8fa7870d48be7fb4f6702fab79
Sebastian Hana <43113824+sebastianlolv@users.noreply.github.com>
2023-02-20 12:19:39 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,136 +1,35 @@
-variable "database_name" {
-  description = "The name of this SQL database."
-  type        = string
+output "server_id" {
+  description = "The ID of this SQL Server."
+  value       = azurerm_mssql_server.this.id
 }
 
-variable "server_name" {
-  description = "The name of this SQL server."
-  type        = string
+output "server_name" {
+  description = "The name of this SQL Server."
+  value       = azurerm_mssql_server.this.name
 }
 
-variable "location" {
-  description = "The location to create the resources in."
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "The name of the resource group to create the resources in."
-  type        = string
-}
-
-variable "storage_account_name" {
-  description = "The name of this Storage account."
-  type        = string
-}
-
-variable "administrator_login" {
+output "administrator_login" {
   description = "The login username of the administrator of this SQL server."
-  type        = string
+  value       = azurerm_mssql_server.this.administrator_login
 }
 
-variable "azuread_administrator" {
-  description = "An Azure AD administrator to configure for this SQL server."
-
-  type = object({
-    login_username              = string
-    object_id                   = string
-    azuread_authentication_only = optional(bool, false)
-  })
-
-  default = null
+output "administrator_password" {
+  description = "The login password of the administrator of this SQL server."
+  value       = azurerm_mssql_server.this.administrator_login_password
+  sensitive   = true
 }
 
-variable "identity" {
-  description = "The identity to configure for this SQL Server."
-
-  type = object({
-    type         = optional(string, "SystemAssigned")
-    identity_ids = optional(list(string), [])
-  })
-
-  default = null
+output "storage_account_id" {
+  description = "The ID of this Storage Account."
+  value       = azurerm_storage_account.this.id
 }
 
-variable "firewall_rules" {
-  description = "A map of firewall rules for this SQL server."
-
-  type = map(object({
-    name             = string
-    start_ip_address = string
-    end_ip_address   = string
-  }))
-
-  default = {
-    "azure" = {
-      name             = "AllowAllWindowsAzureIps"
-      start_ip_address = "0.0.0.0"
-      end_ip_address   = "0.0.0.0"
-    }
-  }
+output "storage_account_name" {
+  description = "The name of this Storage Account."
+  value       = azurerm_storage_account.this.name
 }
 
-variable "storage_container_name" {
-  description = "The name of this Storage Container."
-  type        = string
-  default     = "vulnerability-assessment"
-}
-
-variable "sku_name" {
-  description = "The SKU name of this SQL database."
-  type        = string
-  default     = "Basic"
-}
-
-variable "max_size_gb" {
-  description = "The max size of this SQL database in gigabytes."
-  type        = number
-  default     = null
-}
-
-variable "database_storage_account_type" {
-  description = "The type of Storage account used to store backups for this SQL database."
-  type        = string
-  default     = "Geo"
-}
-
-variable "short_term_retention_policy_retention_days" {
-  description = "The number of days that point-in-time restore backups should be retained."
-  type        = number
-  default     = 7
-}
-
-variable "short_term_retention_policy_backup_interval_in_hours" {
-  description = "The hours between each differential backup. Value has to be 12 or 24."
-  type        = number
-  default     = 12
-}
-
-variable "long_term_retention_policy_weekly_retention" {
-  description = "The duration that weekly long-term backups should be retained. Value must be in an ISO 8601 duration format, e.g. `P1Y`, `P1M`, `P1W` or `P7D`."
-  type        = string
-  default     = "PT0S"
-}
-
-variable "long_term_retention_policy_monthly_retention" {
-  description = "The duration that monthly long-term backups should be retained. Value must be in an ISO 8601 duration format, e.g. `P1Y`, `P1M`, `P4W` or `P30D`."
-  type        = string
-  default     = "PT0S"
-}
-
-variable "long_term_retention_policy_yearly_retention" {
-  description = "The duration that yearly long-term backups should be retained. Value must be in an ISO 8601 duration format, e.g. `P1Y`, `P12M`, `P52W` or `P365D`"
-  type        = string
-  default     = "PT0S"
-}
-
-variable "long_term_retention_policy_week_of_year" {
-  description = "The week of year to take the yearly long-term backup. Value must be between `1` and `52`."
-  type        = number
-  default     = 1
-}
-
-variable "tags" {
-  description = "A mapping of tags to assign to the resources."
-  type        = map(string)
-  default     = {}
+output "database_id" {
+  description = "The ID of this SQL database."
+  value       = module.database.id
 }
